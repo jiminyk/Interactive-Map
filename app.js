404c24e6d1bab0b7b1e1a66a97cdba23871f139e
Christian Gomez <jiminiiik@gmail.com>
2019-06-04 12:44:18 -0400

    Version 1

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 /// TODO: research L.map in docs
 // Initialize the map and set view to chosen coordinates and zoom level
 var map = L.map('map', {
-    crs: L.CRS.Simple
+    crs: L.CRS.Simple,
+    minZoom: 1
 });
 
 // Add map image
 // map is x is 7204px y is 5107px
-var bounds = [[0,0], [1000,1000]];
+var bounds = [[0,0], [708,1000]];
 var image = L.imageOverlay('./assets/map.jpg', bounds).addTo(map);
 
 // fit map to bounds
 map.fitBounds(bounds);
-L.control.zoom();
