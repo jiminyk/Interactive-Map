de2a8e75f4d28eb01fec8bd65e8dc6f8232f9052
Christian Gomez <jiminiiik@gmail.com>
2019-06-04 11:34:39 -0400

    Added map layer and initial bounds

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/// TODO: research L.map in docs
+// Initialize the map and set view to chosen coordinates and zoom level
+var map = L.map('map', {
+    crs: L.CRS.Simple
+});
+
+// Add map image
+// map is x is 7204px y is 5107px
+var bounds = [[0,0], [1000,1000]];
+var image = L.imageOverlay('./assets/map.jpg', bounds).addTo(map);
+
+// fit map to bounds
+map.fitBounds(bounds);
+L.control.zoom();
