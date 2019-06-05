465cff61cb983d9425a446f6d540d6763d5e441a
Christian Gomez <jiminiiik@gmail.com>
2019-06-05 11:52:08 -0400

    separated distribution code from dev code

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/// TODO: research L.map in docs
+// Initialize the map and set view to chosen coordinates and zoom level
+var map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: 0
+});
+
+// Add map image
+// map is x is 7204px y is 5107px
+var bounds = [[0,0], [708,1000]];
+var image = L.imageOverlay('./images/map.jpg', bounds).addTo(map);
+
+// fit map to bounds
+map.fitBounds(bounds);
