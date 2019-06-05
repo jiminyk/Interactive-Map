9e725ff5eb60100e08702a744511acaa41f1bc41
Christian Gomez <jiminiiik@gmail.com>
2019-06-05 10:24:53 -0400

    organized files and using local leaf files

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var map = L.map('map', {
 // Add map image
 // map is x is 7204px y is 5107px
 var bounds = [[0,0], [708,1000]];
-var image = L.imageOverlay('./assets/map.jpg', bounds).addTo(map);
+var image = L.imageOverlay('./images/map.jpg', bounds).addTo(map);
 
 // fit map to bounds
 map.fitBounds(bounds);
